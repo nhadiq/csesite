bb0d9c3d0625603ebfec52b136cab4f25ddb3973
NHadiq <nhadiq@gmail.com>
2019-02-13 23:12:07 +0530

    correction
    finalised

--- js/custom.js
+++ js/custom.js
@@ -158,6 +158,33 @@ function searchbox() {
 	}
 }
 
+//===Language switcher===
+function languageSwitcher() {
+    if ($("#polyglot-language-options").length) {
+        $('#polyglotLanguageSwitcher').polyglotLanguageSwitcher({
+            effect: 'fade',
+            testMode: true,
+            onChange: function(evt) {
+                    alert("The selected language is: " + evt.selectedItem);
+                }
+                //                ,afterLoad: function(evt){
+                //                    alert("The selected language has been loaded");
+                //                },
+                //                beforeOpen: function(evt){
+                //                    alert("before open");
+                //                },
+                //                afterOpen: function(evt){
+                //                    alert("after open");
+                //                },
+                //                beforeClose: function(evt){
+                //                    alert("before close");
+                //                },
+                //                afterClose: function(evt){
+                //                    alert("after close");
+                //                }
+        });
+    };
+}
 
 //===Mixitup Gallery===
 if($('.mixitup-gallery').length){
